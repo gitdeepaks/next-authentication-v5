d13bf1bd2ee9e1026a171f52ac6d786250a62e79
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-04 12:09:20 +0530

    init next-authentication🚀

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Button size="lg" variant="default">
+      Click me
+    </Button>
+  );
+}
