97e23f742083daffac5bd7f5fe8d756e5afaf421
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-08 19:13:41 +0530

    added 22 factor authentication partially 🚀

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,8 +2,17 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+export const sendTwoFactorTokenMail = async (email: string, token: string) => {
+  const conformLink = `http://localhost:3000/auth/verify?token=${token}`;
+};
+
 export const sendPasswordResetMail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = await resend.emails.send({
+    from: "admin@deepaksankhyan.in",
+    to: email,
+    subject: "YOUR 2FA Code",
+    html: `<p>Your 2FA Code is ${token}</p>`,
+  });
 
   await resend.emails.send({
     from: "admin@deepaksankhyan.in",
